9bf308d7cedb8d0cbc12a9289abc21882541eb5f
SandraEliz <117297044+SandraEliz@users.noreply.github.com>
2023-06-16 16:06:39 +0000

    Deploying to gh-pages from  @ a5b4e8fa713b0d413604f8531f08d4ed4da0d6b0 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,80 @@
-// Add any custom javascript here.
+// Add any custom javafuscript here.
+var colors1 = [
+  "#c4e1c6",
+  "#b0d1b3",
+  "#9bc2a1",
+  "#87b28f",
+  "#74a37c",
+  "#60946b",
+  "#4d8559",
+  "#3a7747",
+  "#276836"
+];
+function myMapColorFunction(indicatorId, goalId) {
+  switch(parseInt(goalId)){
+    case 1:
+      return chroma.brewer.OrRd;
+      break;
+     case 2:
+      return chroma.brewer.OrRd;
+      break;
+     case 3:
+      return colors1;
+      break;
+     case 4:
+      return chroma.brewer.OrRd;
+      break;
+     case 5:
+      return chroma.brewer.OrRd;
+      break;
+     case 6:
+      return chroma.brewer.OrRd;
+      break;
+     case 7:
+      return chroma.brewer.OrRd;
+      break;
+     case 8:
+      return chroma.brewer.OrRd;
+      break;
+     case 9:
+      return colors1;
+      break;
+     case 10:
+      return colors1;
+      break;
+     case 11:
+      return colors1;
+      break;
+     case 12:
+      return colors1;
+      break;
+     case 13:
+      return colors1;
+      break;
+     case 14:
+      return colors1;
+      break;
+     case 15:
+      return colors1;
+      break;
+     case 16:
+      return colors1;
+      break;
+     case 17:
+      return colors1;
+      break;
+  }
+}
+function revisaContra() {
+  var contra= document.getElementById("contra").value;
+  console.log(contra);
+  if (contra=="password"){
+    document.getElementById("divContra").style.display="none";
+    document.getElementById("errorContra").style.visibility="hidden";
+  }
+  else {
+    document.getElementById("divContra").style.display="block";
+    document.getElementById("errorContra").style.visibility="visible";
+  }
+  
+}

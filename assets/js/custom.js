0b216c6261e38cec9127637909df051738b0702d
SandraEliz <117297044+SandraEliz@users.noreply.github.com>
2023-06-27 16:45:11 -0600

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -144,7 +144,7 @@ if (URLactual=="https://prep-odsmexico.github.io/site/"){
 else{
   let datoUsuario = JSON.parse(localStorage.getItem('usuario'));
   if (URLactual!="https://prep-odsmexico.github.io/site" + "?usua=" + datoUsuario.userName){
-    window.location.href=URLactual.substring(0, URLactual.length-1) + "?usua=" + datoUsuario.userName";
+    window.location.href=URLactual.substring(0, URLactual.length-1) + "?usua=" + datoUsuario.userName;
   }
 }
 

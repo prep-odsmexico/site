4a4e1edc990d09e7c784c8bb7da215da7ee1b093
SandraEliz <117297044+SandraEliz@users.noreply.github.com>
2023-06-27 21:48:26 +0000

    Deploying to gh-pages from  @ 0d0a420c844cede9e9320026775bc399cf5fbeff 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 // Add any custom javafuscript here.
 //arreglo de usuarios y contraseñas
-
+var URLactual = window.location.href;
 var usucontra=[
   {
     "usuario":"sandra.marquez",
@@ -117,7 +117,7 @@ function revisaContra() {
           document.getElementById("dUsu").style.visibility="hidden";
           document.getElementById("dContra").style.visibility="hidden";
           document.body.style.overflow = "auto";
-          window.location.href=URLactual.substring(0, URLactual.length-1)) + "?usua=" + usu;
+          window.location.href=URLactual.substring(0, URLactual.length-1) + "?usua=" + usu;
         }
       }
     }
@@ -130,12 +130,12 @@ function revisaContra() {
     document.getElementById("dContra").style.visibility="hidden";
   }
 }
-var URLactual = window.location.href;
+
 console.log(URLactual);
 if (URLactual=="https://prep-odsmexico.github.io/site/"){
   document.body.style.overflow = "hidden";
 }
 else{
-  window.location.href=URLactual.substring(0, URLactual.length-1)) + "?usua=sandra.marquez";
+  window.location.href=URLactual.substring(0, URLactual.length-1) + "?usua=sandra.marquez";
 }
 
